9b275cae8bfd1a704d1377bc992d6f94c6d57a73
mohammedazan <mohammedazain0@gamil.com>
2025-05-28 17:01:29 +0100

    Initialize Django project structure with essential files and configurations

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for gradientviz project.
+URL configuration for src project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
+    path('App/', include('App.urls')),
     path('admin/', admin.site.urls),
-    path('app/', include('App.urls')),
 ]
